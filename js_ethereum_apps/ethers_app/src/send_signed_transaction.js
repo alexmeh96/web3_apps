f9241280dec07afd797bf4ab109e17f65f907716
Alexandr Meshkov <alex.meh96@mail.ru>
2024-02-11 19:55:45 +0300

    js_ethereum_app ethers_app: add send_signed_transaction.js

---
+++ js_ethereum_apps/ethers_app/src/send_signed_transaction.js
@@ -0,0 +1,34 @@
+const { ethers } = require("ethers");
+
+const INFURA_ID = ''
+const provider = new ethers.JsonRpcProvider(`https://kovan.infura.io/v3/${INFURA_ID}`)
+
+const account1 = '' // Your account address 1
+const account2 = '' // Your account address 2
+
+const privateKey1 = '' // Private key of account 1
+const wallet = new ethers.Wallet(privateKey1, provider)
+
+const main = async () => {
+    const senderBalanceBefore = await provider.getBalance(account1)
+    const recieverBalanceBefore = await provider.getBalance(account2)
+
+    console.log(`\nSender balance before: ${ethers.formatEther(senderBalanceBefore)}`)
+    console.log(`reciever balance before: ${ethers.formatEther(recieverBalanceBefore)}\n`)
+
+    const tx = await wallet.sendTransaction({
+        to: account2,
+        value: ethers.parseEther("0.025")
+    })
+
+    await tx.wait()
+    console.log(tx)
+
+    const senderBalanceAfter = await provider.getBalance(account1)
+    const recieverBalanceAfter = await provider.getBalance(account2)
+
+    console.log(`\nSender balance after: ${ethers.formatEther(senderBalanceAfter)}`)
+    console.log(`reciever balance after: ${ethers.formatEther(recieverBalanceAfter)}\n`)
+}
+
+main()
