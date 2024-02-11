c3fd940a6cefdb3aab480b4d8c82ef49eee50af3
Alexandr Meshkov <alex.meh96@mail.ru>
2024-02-11 21:46:45 +0300

    js_ethereum_app wagmi_app: add accounts.js

---
+++ js_ethereum_apps/wagmi_app/src/accounts.js
@@ -0,0 +1,27 @@
+import {createConfig, getBalance, getBlock, http} from '@wagmi/core'
+import {sepolia} from '@wagmi/core/chains'
+
+const INFURA_ID = ''
+
+const config = createConfig({
+    // chains: [mainnet, sepolia],
+    chains: [sepolia],
+    transports: {
+        [sepolia.id]: http(`https://sepolia.infura.io/v3/${INFURA_ID}`),
+        // [mainnet.id]: http(`https://mainnet.infura.io/v3/${INFURA_ID}`),
+    },
+})
+
+const main = async () => {
+
+    const blockNumber = await getBlock(config)
+
+    const balance = await getBalance(config, {
+        address: '0xcD00aC6C1d67ebD195BC296B65a29E8196dF2DAb',
+    })
+
+    console.log(`blockNumber: ${blockNumber.number}`)
+    console.log(`balance: ${balance.value}`)
+}
+
+main()
